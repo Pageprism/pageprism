2fe26ddf54bc1913c2c92ff2616629727630aee6
David Consuegra <david@>
2015-08-24 15:07:21 +0300

    Clean up javascript code a bit

--- application/views/footer.php
+++ application/views/footer.php
@@ -26,12 +26,6 @@
 	$( document ).ready(function() {
 
 
-        // "Back to shelves"
-		$('#scroll-to-top').waypoint('sticky');
-		$('#scroll-to-top').click( function() {
-			$("html, body").animate({ scrollTop: 0 }, "fast")	
-		})
-
 
         function load_page_content(id,page_n,div_id,direction,clicked) {
             if (direction == "up") {
@@ -207,101 +201,6 @@
         });
 
     });
-
-        // Page share elements
-
-        // Facebook
-        $(document).on('click','.share-fb', function(){
-            event.preventDefault ? event.preventDefault() : event.returnValue = false;
-            url = $(this).attr("href");
-            title = $(this).attr("rel");
-            image = window.location.protocol+'//'+window.location.host+"/"+$("#page_1 .rendered-page-single").attr("src");
-            id = $(".publication-header .book-id-hidden").html();
-
-            FB.ui(
-                {
-                    method: 'feed',
-                    name: title,
-                    link: url,
-                    picture: image,
-                    caption: "eSamiszat-Shelf",
-                    description: "Read more...",
-                    message: ''
-                },
-                function(response) {
-                    if (response && response.post_id) {
-                    var form_data = {
-                        id : id
-                    }
-
-                    $.ajax({
-                        url: "/index.php/book/counter",
-                        type: 'POST',
-                        async: true,
-                        data: form_data,
-                        success: function(data) {
-                        }
-                    });
-                    }
-              });
-        });   
-
-        // Google+
-        $(document).on('click','.share-google', function(){
-            event.preventDefault ? event.preventDefault() : event.returnValue = false;
-            window.open(this.href,'share_window', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-            id = $(".publication-header .book-id-hidden").html();
-            var form_data = {
-                id : id
-            }
-
-            $.ajax({
-                url: "/index.php/book/counter",
-                type: 'POST',
-                async: true,
-                data: form_data,
-                success: function(data) {
-                }
-            });
-        });
-
-        // Twitter
-        $(document).on('click','.share-twitter', function(){
-            id = $(".publication-header .book-id-hidden").html();
-            var form_data = {
-                id : id
-            }
-
-            $.ajax({
-                url: "/index.php/book/counter",
-                type: 'POST',
-                async: true,
-                data: form_data,
-                success: function(data) {
-                }
-            });
-        });
-
-		// Click "Show Covers"
-		$('.control-covers').click( function() {
-            event.preventDefault();
-			$('.control-table').removeClass('active');
-			$('.control-covers').addClass('active');
-			$('#covers').removeClass('list-table');
-			$('#covers').addClass('list-cover');
-            $('.single-cover img').css({width: '75', height: '110'})
-		});
-
-		// Click "Show downloads"
-		$('.control-table').click( function() {
-            event.preventDefault();
-			$('.control-covers').removeClass('active');
-			$('.control-table').addClass('active');
-			$('#covers').removeClass('list-cover');
-			$('#covers').addClass('list-table');
-            $('.single-cover img').css({width: '35', height: '51'})
-		});
-
 
     });
 	</script>
--- assets/js/shelf.js
+++ assets/js/shelf.js
@@ -1,25 +1,100 @@
 $(function() {
 
+  // "Back to shelves"
+  $('#scroll-to-top').waypoint('sticky');
+  $('#scroll-to-top').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "fast")	
+  });
+  $(window).scroll(lazyload);
+  lazyload();
 
+  function lazyload(){
+    var wt = $(window).scrollTop();    //* top of the window
+    var wb = wt + $(window).height();  //* bottom of the window
 
-   $(window).scroll(lazyload);
-   lazyload();
+    $(".single-page").each(function(){
+      var ot = $(this).offset().top;  //* top of object (i.e. advertising div)
+      var ob = ot + $(this).height(); //* bottom of object
 
-   function lazyload(){
-	   var wt = $(window).scrollTop();    //* top of the window
-	   var wb = wt + $(window).height();  //* bottom of the window
+      if(!$(this).attr("loaded") && wt<=ob && wb >= ot){
+        //$(this).html("here goes the iframe definition");
+        $(this).attr("loaded",true);
+      }
+    });
+  }
+  var href = location.pathname;
 
-	   $(".single-page").each(function(){
-	      var ot = $(this).offset().top;  //* top of object (i.e. advertising div)
-	      var ob = ot + $(this).height(); //* bottom of object
+  // Page share elements
+  // Facebook
+  $(document).on('click','.share-fb', function(){
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    url = $(this).attr("href");
+    title = $(this).attr("rel");
+    image = window.location.protocol+'//'+window.location.host+"/"+$("#page_1 .rendered-page-single").attr("src");
+    id = $(".publication-header .book-id-hidden").html();
 
-	      if(!$(this).attr("loaded") && wt<=ob && wb >= ot){
-	         //$(this).html("here goes the iframe definition");
-	         $(this).attr("loaded",true);
-	      }
-	   });
-	}
-	var href = location.pathname;
-//window.alert(href.substr(href.lastIndexOf('/') + 1));
+    FB.ui(
+      {
+      method: 'feed',
+      name: title,
+      link: url,
+      picture: image,
+      caption: "eSamiszat-Shelf",
+      description: "Read more...",
+      message: ''
+    },
+    function(response) {
+      if (response && response.post_id) {
+        var form_data = {
+          id : id
+        }
+
+        $.ajax({
+          url: "/index.php/book/counter",
+          type: 'POST',
+          async: true,
+          data: form_data,
+          success: function(data) {
+          }
+        });
+      }
+    });
+  });   
+
+  // Google+
+  $(document).on('click','.share-google', function(){
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    window.open(this.href,'share_window', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+    id = $(".publication-header .book-id-hidden").html();
+    var form_data = {
+      id : id
+    }
+
+    $.ajax({
+      url: "/index.php/book/counter",
+      type: 'POST',
+      async: true,
+      data: form_data,
+      success: function(data) {
+      }
+    });
+  });
+
+  // Twitter
+  $(document).on('click','.share-twitter', function(){
+    id = $(".publication-header .book-id-hidden").html();
+    var form_data = {
+      id : id
+    }
+
+    $.ajax({
+      url: "/index.php/book/counter",
+      type: 'POST',
+      async: true,
+      data: form_data,
+      success: function(data) {
+      }
+    });
+  });
 
 });
