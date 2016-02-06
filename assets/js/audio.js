40306f6f6f7b92c22f450e1fb41058981c5acae1
David Consuegra <david@>
2016-02-06 14:06:05 +0200

    Moved to a event based model for audio and playlist handling

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -5,42 +5,70 @@
   loadAudio = function(elements) {
     audiojs.events.ready(function() {
       elements.filter(":not(.audioloaded)").addClass('audioloaded').each(function() {
-        var ajs = audiojs.create(this, {
+
+        //Prepare audiojs settings with triggers for jquery events
+        var settings = {
           css: null,
-          play: onPlay,
-          trackEnded: advanceTrack
+        };
+        var eventNames = ['trackEnded', 'flashError', 'loadError', 'init', 'loadStarted', 'play', 'pause'];
+        $.each(eventNames, function(i, eventName) {
+          settings[eventName] = function() {
+            //Replicate audiojs internal stuff
+            audiojs.settings[eventName].apply(this);
+            $(this.element).trigger('audiojs:'+eventName, this);
+          };
         });
+
+        //Init audiojs
+        var ajs = audiojs.create(this, settings);
+
         //We need to access the element cloned by audiojs instead of the original :'(
         $(ajs.element).data('audiojs', ajs);
       });
     });
-  }
+  };
   
-  function onPlay() {
-    //Debug purposes
-    currentlyPlayingAudio = this;
-    //Replicate audiojs internal stuff
-    var player = this.settings.createPlayer;
-    audiojs.helpers.addClass(this.wrapper, player.playingClass);
-
-    if (currentAudio !== null && currentAudio != this) {
+  $(document).on('playlist:playPause', function() {
+    var playing = currentAudio && currentAudio.playing;
+    $(document).trigger(playing ? 'playlist:pause' : 'playlist:play');
+  });
+  $(document).on('playlist:play', function() {
+    if (currentAudio) {
+      currentAudio.play();
+    } else {
+      var firstTrack = $('audio:eq(0)');
+      if (firstTrack.length === 0) return;
+      
+      firstTrack.data('audiojs').play();
+    }
+  });
+  $(document).on('playlist:pause', function() {
+    if (currentAudio) {
       currentAudio.pause();
     }
-    currentAudio = this;
-  }
-  function advanceTrack() {
-    var currentAudio = this.element;
+  });
+  $(document).on('audiojs:play', function(event, audiojs) {
+    //Debug purposes
+    currentlyPlayingAudio = audiojs;
+
+    if (currentAudio !== null && currentAudio != audiojs) {
+      currentAudio.skipTo(0);
+      currentAudio.pause();
+    }
+    currentAudio = audiojs;
+  });
+  $(document).on('audiojs:trackEnded', function(event, audiojs) {
+    var currentAudio = audiojs.element;
     var audios = $('audio');
     var nextIndex = audios.index(currentAudio) + 1;
     
     if (nextIndex >= audios.length) {
       nextIndex = 0;
     }
-    if (nextIndex == 0) return;
     
     var nextAudio = audios.eq(nextIndex).data('audiojs');
-    nextAudio.seekTo(0);
+    nextAudio.skipTo(0);
     nextAudio.play();
-  }
+  });
 
 })();
