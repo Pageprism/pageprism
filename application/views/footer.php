49f149606d68141bc0d879b6ae7db3f20952ae90
David Consuegra <david@>
2016-03-18 22:29:37 +0200

    Asset version number bump

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,8 +5,8 @@
     <script src="<?= base_url();?>assets/js/perfect-scrollbar.jquery.min.js"></script>
     <script src="<?= base_url();?>assets/audiojs/audio.min.js"></script>
     <script src="<?= base_url();?>assets/js/audio.js?v=0"></script>
-    <script src="<?= base_url();?>assets/js/shelf.js?v=6c"></script>
-    <script src="<?= base_url();?>assets/js/navi.js?v=2"></script>
+    <script src="<?= base_url();?>assets/js/shelf.js?v=7"></script>
+    <script src="<?= base_url();?>assets/js/navi.js?v=3"></script>
     <?php if (isset($current_book)):  ?>
     <script>
       $(document).ready(function() {
