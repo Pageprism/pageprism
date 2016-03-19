2037d197b84e38546a6092a322817f4491d0ff13
David Consuegra <david@>
2016-03-19 11:51:15 +0200

    Fix login route usage

--- application/views/admin/auth.php
+++ application/views/admin/auth.php
@@ -1,6 +1,6 @@
 <div id="login">
   <h1>Authenticate</h1>
-  <?php echo form_open('admin/auth?backUrl='.urlencode($backUrl));?>
+  <?php echo form_open('login?backUrl='.urlencode($backUrl));?>
   <p><label>Username:</label> <?php echo form_input('user');?></p>
   <p><label>Password:</label> <?php echo form_password('password');?></p>
   <?php echo form_hidden('posted', 'true');?>
