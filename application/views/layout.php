7ba28754fc3501a07cd595000dcd69173260bd32
David Consuegra <david@>
2016-06-19 15:20:36 +0300

    Remove unused font

--- application/views/layout.php
+++ application/views/layout.php
@@ -7,28 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <?php $this->load->helper('asset'); ?>
 
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" media="screen" />
     <link href="<?php echo base_url();?>assets/css/perfect-scrollbar.css" rel="stylesheet" media="screen" />
-    <link href="<?php echo base_url();?>assets/css/font-awesome.min.css" rel="stylesheet">
     <?= load_stylesheet('esamizdat'); ?>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
     <link rel="shortcut icon" href="<?php echo base_url();?>assets/img/esamizdat.ico" />
     <link href='<?= base_url(); ?>' rel='top'>
     <?php if (isset($cover_image)): ?>
     <meta property="og:image" content="<?= base_url(), $cover_image; ?>" />
     <?php endif; ?>
 
-    <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-41565206-1']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-    </script>
 
   </head>
   <body>
@@ -76,7 +64,7 @@
     }
     </script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js"></script>
     <script src="<?= base_url();?>assets/js/perfect-scrollbar.jquery.min.js"></script>
     <script src="<?= base_url();?>assets/audiojs/audio.min.js"></script>
@@ -93,31 +81,39 @@
     });
     </script>
     <?php endif; ?>
-    <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-41565206-1', 'auto');
-    ga('send', 'pageview');
-    </script>
     <div id="fb-root"></div>
-    <script>
-    window.fbAsyncInit = function() {
-      FB.init({
-      appId : '366708340107485',
-        status : true, // check login status
-        cookie : true, // enable cookies to allow the server to access the session
-        xfbml : true // parse XFBML
-      });
-    };
+    <script type="text/javascript">
+    $(document).ready(function() { setTimeout(function() {
+      window.fbAsyncInit = function() {
+        FB.init({
+        appId : '366708340107485',
+          status : true, // check login status
+          cookie : true, // enable cookies to allow the server to access the session
+          xfbml : true // parse XFBML
+        });
+      };
 
-    (function() {
-      var e = document.createElement('script');
-      e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-      e.async = true;
-      document.getElementById('fb-root').appendChild(e);
-    }());
+      (function() {
+        var e = document.createElement('script');
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        e.async = true;
+        document.getElementById('fb-root').appendChild(e);
+      }());
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-41565206-1', 'auto');
+      ga('send', 'pageview');
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-41565206-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    }, 2500); });
     </script>
   </body>
 </html>
